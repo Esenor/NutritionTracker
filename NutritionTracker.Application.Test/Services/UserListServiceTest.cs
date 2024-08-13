96e52f0b807df137b06fadc26fe4cdb081c194b8
David Dieu <daviddieu80@gmail.com>
2024-08-13 11:48:57 +0200

    Add new NutritionTracker.Application.Test project with Xunit

---
+++ NutritionTracker.Application.Test/Services/UserListServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NutritionTracker.Application.Services;
+using NutritionTracker.Domain.Entities;
+using NutritionTracker.Infrastructure.Database.Repositories.Interfaces;
+
+namespace NutritionTracker.Application.Test.Services
+{
+    public class UserListServiceTest
+    {
+        [Fact]
+        public async Task List()
+        {
+            Mock<IUserRepository> mockUserRepository = new();
+
+            IEnumerable<User> inputUsers = [
+                new User(1, "admin@localhost", "azeccc", "zeiouzz", ["ADMIN", "USER"], true),
+                new User(2, "user@localhost", "qsdqf", "oiuzeghiozeh", ["USER"], false),
+            ];
+
+            IEnumerable<User> expectedUsers = [
+                new User(1, "admin@localhost", "azeccc", "zeiouzz", ["ADMIN", "USER"], true),
+                new User(2, "user@localhost", "qsdqf", "oiuzeghiozeh", ["USER"], false),
+            ];
+
+            mockUserRepository.Setup(r => r.ListAll()).ReturnsAsync(inputUsers);
+
+            UserListService service = new UserListService(mockUserRepository.Object);
+
+            IEnumerable<User> actualUsers = await service.List();
+
+            Assert.Equivalent(expectedUsers, actualUsers);
+        }
+    }
+}
